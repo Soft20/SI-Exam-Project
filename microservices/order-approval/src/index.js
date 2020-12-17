7963bc4cbf76b182f00899c37f9cf7d0cbda59fe
Stephan-MDD <stephan.mdd@gmail.com>
2020-12-17 20:56:09 +0100

    added camunda service

---
+++ microservices/order-approval/src/index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const fetch = require('node-fetch');
+require('./camunda');
+
+const PORT = 3000;
+const CAMUNDA_HOST = 'http://localhost:8080/engine-rest/process-definition/key/order-approval/start';
+
+const app = express();
+app.use(express.json());
+
+app.post('/purchase', async (req, res) => {
+	const { orderId, price, mail } = req.body;
+
+	const body = {
+		variables: {
+			orderId: { value: orderId, type: 'string' },
+			customer: { value: mail, type: 'string' },
+			price: { value: price, type: 'long' },
+		},
+	};
+
+	const options = {
+		method: 'POST',
+		mode: 'cors',
+		cache: 'no-cache',
+		headers: { 'Content-Type': 'application/json' },
+		body: JSON.stringify(body),
+	};
+
+	const response = await fetch(CAMUNDA_HOST, options);
+	const json = await response.json();
+	console.log('JSON::', json);
+
+	res.json({ message: `Order ${orderId} received and is being processed.` });
+});
+
+app.listen(PORT, () => console.log(`BPMN Service running at http://localhost:${PORT}`));
